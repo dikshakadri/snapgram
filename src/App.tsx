16d2758e0ad18b09e72470033a235563583b70fa
Deeksha Kishore <deekshakishore@Deekshas-MacBook-Air.local>
2024-12-25 14:49:13 +0530

    Code with Topbar, Sidebar and Bottombar in home page with logout functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, LikedPost, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import { Toaster } from "@/components/ui/toaster"
 
 const App = () => {
@@ -20,6 +20,15 @@ const App = () => {
             {/* private routes. index in the route means this is the starting page */}
             <Route element={<RootLayout />}>
                 <Route index element={<Home />} />
+                <Route path="/explore" element={<Explore />} />
+                <Route path="/saved" element={<Saved />} />
+                <Route path="/all-users" element={<AllUsers />} />
+                <Route path="/create-post" element={<CreatePost />} />
+                <Route path="/update-post" element={<EditPost />} />
+                <Route path="/liked-post" element={<LikedPost />} />
+                <Route path="/posts/:id" element={<PostDetails />} />
+                <Route path="/profile/:id/*" element={<Profile />} />
+                <Route path="/update-profile/:id" element={<UpdateProfile />} />
             </Route>
         </Routes>
         <Toaster />
---
+++ src/_root/pages/LikedPost.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LikedPost = () => {
+  return (
+    <div>LikedPost</div>
+  )
+}
+
+export default LikedPost
+
